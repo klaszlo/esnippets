87b1c0bcec525e734225bb492434dc3601d0175b
Laszlo <laszlo.krekacs.list@gmail.com>
2009-10-19 19:54:00 +0200

    scroller try. (does not work)

--- playground/swallowd_entry/sentry.py
+++ playground/swallowd_entry/sentry.py
@@ -12,6 +12,14 @@ ly.file_set("people.edj", "CreateContact")
 win.resize_object_add(ly)
 ly.show()
 
+scroller = elementary.Scroller(win)
+scroller.size_hint_weight_set(1.0, 1.0)
+scroller.size_hint_align_set(-1.0, -1.0)
+scroller.bounce_set(0, 0)
+scroller.show()
+
+
+
 textbox = elementary.Entry(win)
 textbox.single_line_set(True)
 textbox.color_set(255, 255, 255, 255)
@@ -22,9 +30,11 @@ textbox.size_hint_align_set(-1.0, -1.0)
 
 #textbox.size_hint_min_set(400,400)
 
+scroller.content_set(textbox)
+scroller.show()
 
 
-ly.content_set('entry2', textbox)
+ly.content_set('entry2', scroller)
 win.resize(480, 640)
 win.show()
 elementary.run()
