34296451418ffcc485e87c70eb7a29d7f55090ec
Laszlo <laszlo.krekacs.list@gmail.com>
2009-10-19 19:47:44 +0200

    Sachiel's idea

--- playground/swallowd_entry/sentry.py
+++ playground/swallowd_entry/sentry.py
@@ -1,21 +1,32 @@
 
 import elementary
-elementary.init()
 import edje
 import evas
+
+elementary.init()
+
 win = elementary.Window("inject", elementary.ELM_WIN_BASIC)
 win.autodel_set(True)
 ly = elementary.Layout(win)
 ly.file_set("people.edj", "CreateContact")
 win.resize_object_add(ly)
 ly.show()
+
 textbox = elementary.Entry(win)
 textbox.single_line_set(True)
 textbox.color_set(255, 255, 255, 255)
 textbox.entry_set("lolza")
+
 textbox.size_hint_weight_set(1.0, 1.0)
+textbox.size_hint_align_set(-1.0, -1.0)
+
+#textbox.size_hint_min_set(400,400)
+
+
+
 ly.content_set('entry2', textbox)
 win.resize(480, 640)
 win.show()
 elementary.run()
+
 
